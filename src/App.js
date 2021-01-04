25f3b50de352c5ecad9e5d4bf361679767d9f4c2
VictoriaMarjina <61832815+VictoriaMarjina@users.noreply.github.com>
2021-01-04 20:00:44 +0400

    ws

--- src/App.js
+++ src/App.js
@@ -4,57 +4,44 @@ const path = require('path');
 class App {
     constructor(db) {
         this._db = db;
-
         this._app = express();
         this._app.use(express.json());
         this._app.use('/', express.static(path.resolve(__dirname, '../public')));
 
-        this._app.use(this.headerCors);
-
-        this._app.post('/addUser', this.onChekUser);
-        this._app.get('/getChat', this.onGetChat);
-        this._app.post('/addMessage', this.addNewMessage);
-        this._app.delete('/deleteUser', this.userOff);
+        this._app.get('/getAllMessages', this.getAllMessages);
+        // this._app.post('/addMessage', this.addMessage);
+        this._app.post('/logIn', this.logIn);
+        this._app.delete('/logOut', this.logOut);
     }
+
+    getAllMessages = (req, res) => {
+        const messages = this._db.getMessages();
+
+        res.send(messages);
+        res.end();
+    };
+
+    // addMessage = (req, res) => {
+    //     const { body } = req;
+
+    //     this._db.addNewMessage(body);
+    //     res.end();
+    // };
+
+    logOut = (req, res) => {
+        const { user } = req.body;
+
+        this._db.deleteUserFromDb(user);
+        res.end();
+    };
+
+    logIn = (req, res) => {
+        const { user } = req.body;
+        res.send(this._db.addUserToDb(user));
+        res.end();
+    };
 
     getApp = () => this._app;
-
-    onGetChat = (req, res) => {
-        const chat = this._db.getChat();
-
-        res.send(chat);
-        res.end();
-    }
-
-    onChekUser = (req, res) => {
-        const { userName } = req.body;
-        const check = this._db.getUser(userName);
-
-        res.send(check);
-        res.end();
-    }
-
-    addNewMessage = (req, res) => {
-        const { body } = req;
-
-        this._db.setMessage(body);
-
-        res.end();
-    }
-
-    userOff = (req, res) => {
-        const { body } = req;
-        this._db.deleteUser(body);
-
-        res.end();
-    }
-
-    headerCors = (req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.header("Access-Control-Allow-Methods", "GET, POST, DELETE");
-        next();
-    }
 }
 
 module.exports = App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 const App = require('./App');
 const Server = require('./Server');
-
 const Model = require('./model/Model');
 
 function init() {
-    const port = 2021 || process.env.PORT;
-
+    const port = 3002 || process.env.PORT;
     const db = new Model();
     const app = new App(db);
-    const server = new Server(port, app)
+    const server = new Server(port, app, db);
 
     server.start();
 }
